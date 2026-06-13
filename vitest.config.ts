c88f666f24adf55b88b3bce333b11114e7c2e88a
charlotte <charlotte@anyask.ai>
2026-06-14 00:57:27 +0800

    test: include integration tests in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    include: ["src/tests/unit/**/*.test.ts"],
+    include: ["src/tests/unit/**/*.test.ts", "src/tests/integration/**/*.test.ts"],
   },
   resolve: {
     alias: {
